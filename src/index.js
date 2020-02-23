d17fef1bd38b602875bea58b556a2f5d21f02473
Phuoc Le <phuoc.le@tum.de>
2020-02-23 12:07:33 +0100

    add prettier formatting

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import { render } from "react-dom";
 
 const App = () => {
-  const groups = browser.storage.sync.get('groups')
+  const groups = browser.storage.sync.get("groups");
   groups.then(res => {
-    console.log(res)
-  })
+    console.log(res);
+  });
 
-  return <div><button>+</button></div>
-}
+  return (
+    <div>
+      <button>+</button>
+    </div>
+  );
+};
 
 render(<App />, document.querySelector("#app"));
--- src/toolbar.js
+++ src/toolbar.js
@@ -1,10 +1,9 @@
-
 const homeURL = browser.runtime.getURL("dist/index.html");
 
 function openPage() {
-    browser.tabs.update({
-      url: homeURL
-    });
-  }
-  
+  browser.tabs.update({
+    url: homeURL
+  });
+}
+
 browser.browserAction.onClicked.addListener(openPage);
