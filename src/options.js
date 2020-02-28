b3658f627fce39afeb90dce9d44cb099397e5aa9
Phuoc Le <phuoc.le@tum.de>
2020-02-28 11:30:10 +0100

    add version to optionspage

--- src/options.js
+++ src/options.js
@@ -4,6 +4,10 @@ function init() {
   const urlDiv = document.querySelector("#homepage-url");
   const homeURL = browser.runtime.getURL("dist/index.html");
   urlDiv.innerHTML = `<a href="${homeURL}">${homeURL}</a>`;
+
+  const versionDiv = document.querySelector("#version-id");
+  const version = browser.runtime.getManifest().version;
+  versionDiv.innerHTML = `<span>${version}</span>`;
 }
 
 document.onload = init();
