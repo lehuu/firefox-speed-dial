b39da292dc83217ba7806240c10ad23490a9d9a3
Phuoc Le <phuoc.le@tum.de>
2021-12-30 22:05:47 +0100

    refactor options and toolbar to typescript

--- src/extension/options.ts
+++ src/extension/options.ts
@@ -3,11 +3,18 @@ import "./options.css";
 function init() {
   const urlDiv = document.querySelector("#homepage-url");
   const homeURL = browser.runtime.getURL("index.html");
-  urlDiv.innerHTML = `<a href="${homeURL}">${homeURL}</a>`;
-
+  if (urlDiv) {
+    urlDiv.innerHTML = `<a href="${homeURL}">${homeURL}</a>`;
+  }
   const versionDiv = document.querySelector("#version-id");
   const version = browser.runtime.getManifest().version;
-  versionDiv.innerHTML = `<span>${version}</span>`;
+  if (versionDiv) {
+    versionDiv.innerHTML = `<span>${version}</span>`;
+  }
 }
 
-document.onload = init();
+window.onload = () => {
+  init();
+};
+
+export {};
